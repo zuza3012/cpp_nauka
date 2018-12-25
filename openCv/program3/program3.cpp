4aee1626d8fb96836dfa70247131ff22863f391d
Zuza <https://github.com/zuza3012>
2018-12-26 00:06:20 +0100

    mała poprawka

--- openCv/program3/program3.cpp
+++ openCv/program3/program3.cpp
@@ -21,9 +21,10 @@ int main (int argc, char** argv){
 	int b;
 	
 	cin >> a >> b;
+	int licznik = 0;
 	
 	for (int i = 1; i < argc; i++){
-		int licznik = 0;
+		
 		String zapiszZdjecie = "./zdj_" + to_string(++licznik) + ".jpg";
 		
 		string nazwaZdjecia = argv[i];//jak for od 0ra to tutaj argv[i+1], zeby brac obraz, nie program caly, SYPIE SIE
@@ -31,9 +32,11 @@ int main (int argc, char** argv){
 		zdjecie = imread( nazwaZdjecia, 1);
 		
 		Mat zdj_pomn;
-		
+	
+
 		resize (zdjecie, zdj_pomn, Size(a,b), 0, 0, CV_INTER_LINEAR);//Szie(width, height)// cos tu moze jest zle?
-		
+		 
+		 
 		//vector <Mat> wyj;
 		//wyj[i] = zdj_pomn.clone();
 		imwrite (zapiszZdjecie, zdj_pomn);//wyj[i]);
---
+++ openCv/program4/program4.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/opencv.hpp>
+#include <string>
+#include <iostream>
+
+int main (int argc, char** argv){
+	
+	
+	return 0;
+}
