77a1653140646a9ce884a0050550176bb6843003
Zuza <https://github.com/zuza3012>
2018-12-26 00:28:39 +0100

    drobne poprawki

--- openCv/program3/program3.cpp
+++ openCv/program3/program3.cpp
@@ -21,13 +21,14 @@ int main (int argc, char** argv){
 	int b;
 	
 	cin >> a >> b;
-	int licznik = 0;
+	
 	
 	Mat zdjecie;
 	Mat zdj_pomn;
+	int licznik = 0;
 	
 	for (int i = 1; i < argc; i++){
-		
+		//static int licznik = 0; //raz zainicjalizowany + ponad tym blokiem
 		String zapiszZdjecie = "./zdj_" + to_string(++licznik) + ".jpg";
 		
 		string nazwaZdjecia = argv[i];//jak for od 0ra to tutaj argv[i+1], zeby brac obraz, nie program caly, SYPIE SIE
@@ -37,8 +38,10 @@ int main (int argc, char** argv){
 		resize (zdjecie, zdj_pomn, Size(a,b), 0, 0, CV_INTER_LINEAR);//Szie(width, height)// cos tu moze jest zle?
 		 
 		imwrite (zapiszZdjecie, zdj_pomn);
-		cout << "Pomniejszono obraz nr" << i << endl;
+		cout << "Zmiana rozzdielczosci obrazu nr" << i << endl;
 	}
+	
+	
 	
 	return 0;
 	
